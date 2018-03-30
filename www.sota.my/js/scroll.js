e031b27ef962f66e931ef489ab84e8a098657d6a
Аптека <admin@dp-malish.com>
2018-03-30 14:50:16 +0300

    Top menu 1.0

--- www.sota.my/js/scroll.js
+++ www.sota.my/js/scroll.js
@@ -1,4 +1,15 @@
-
+window.onscroll=function(){
+    if(window.pageYOffset>0&&window.pageYOffset<310&&document.body.clientWidth>800){
+        document.getElementById('headcontact').style.position="fixed";
+        document.getElementById('headcontact').style.width="100%";
+        document.getElementById('headcontact').style.margin="auto";
+        document.getElementById('headcontact').style.left=0;
+        document.getElementById('headcontact').style.right=0;
+    }else{
+        document.getElementById('headcontact').style.position="relative";
+        //alert(window.pageYOffset);
+    }
+};
 
 
 
