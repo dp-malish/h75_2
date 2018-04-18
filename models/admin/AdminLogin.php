66fcfaf9e0680f1019a382ca7eec94f5278fd819
Alexandr <admin@dp-malish.com>
2018-04-18 15:42:23 +0300

    -=Sota=- Start v 0.05 -=Stroy=- Start v 0.7 Img v 0.5 Admin v0.1

---
+++ models/admin/AdminLogin.php
@@ -0,0 +1,8 @@
+<div class="fon_c"><h4>Вход администратора</h4>
+    <form id="" class="form" method="post">
+        <p>Логин:</p>
+        <input type="text" name="name" required placeholder="Логин *" maxlength="100">
+        <p>Пароль:</p>
+        <input type="password" name="pass" required placeholder="Пароль *" maxlength="100">
+        <input type="submit" value="отправить"></form>
+<p class="note">Примечание: поля, помеченные * звездочкой - обязательны для заполнения</p></div>
