0365df3ebef5d714ad5b9475f2acb7e985bb0919
Alexandr <admin@dp-malish.com>
2018-05-17 20:42:27 +0300

    -=Sota=- Start v 0.05 -=Stroy=- Start v 0.85 Img v 1.0 Admin v0.4 Sitemap v0.85 contacts v0.7 -=Vt2=- Start v0.4 Авторизация v0.75 Top_menu v1.0 Fish v1.0 Left_menu v0.05

--- incl/vt/def/viewarticle.php
+++ incl/vt/def/viewarticle.php
@@ -17,20 +17,10 @@ class ViewArticle{
       Def\Opt::$title=$res['title'];
       Def\Opt::$description=$res['meta_d'];
       Def\Opt::$keywords=$res['meta_k'];
-
       if($res['img']!=''){$img='<img class="fl five img_link" src="'.SqlTable::getImgDirTable($table_name_img).$res['img'].'" alt="'.$res['img_alt'].'" title="'.$res['img_title'].'">';}else{$img='';}
-
-
       if($button_back!=''){
-        $button_back='<p><a href="/'.Def\Route::$uri_parts[0].'/" onclick="button_back(\'обзор/\');return false;" rel="nofollow">&#9668;&mdash;</a><br></p>';
-
-
-      }
-
-
-
-
-      Def\Opt::$main_content.='<section><div class="fon_c"><article><h3>'.$res['caption'].'</h3><div class="cl"></div>'.$button_back.$img.$res['full_text'].'<p><a href="/'.Def\Route::$uri_parts[0].'/" onclick="button_back(\'обзор/\');return false;" rel="nofollow">&#9668;&mdash; Вернуться к меню обзоров</a></p></article><div class="cl"></div></div></section>';
+        $button_back='<p><a href="/'.Def\Route::$uri_parts[0].'/" onclick="button_back(\'обзор/\');return false;" rel="nofollow">&#9668;&mdash;</a><br></p>';}
+      Def\Opt::$main_content.='<section><div class="fon_c"><article><h3>'.$res['caption'].'</h3><div class="cl"></div>'.$button_back.$img.$res['full_text'].'<p>'.$button_back.'</p></article><div class="cl"></div></div></section>';
     }else{Def\Route::$module404=true;}
   }
 
