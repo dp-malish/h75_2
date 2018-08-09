5459019ba0dbdc52372ad2293e05686a986c32ad
Дом <admin@dp-malish.com>
2018-08-10 00:06:20 +0300

    win2 start 0.1

--- www.win2.my/index.php
+++ www.win2.my/index.php
@@ -3,7 +3,8 @@ namespace lib\Def;
 Error_Reporting(E_ALL & ~E_NOTICE);ini_set('display_errors',1);
 set_include_path(get_include_path().PATH_SEPARATOR.'../');spl_autoload_register();
 $Opt=new Opt('win');//Def opt
-$Cash=new Cache_File(['win'],true);
+
+Cache_File::$cash=new Cache_File(['win'],true);
 
 $bot=new UserAgent();
 
@@ -11,12 +12,12 @@ $bot=new UserAgent();
 
 if($_SERVER['REQUEST_URI']!='/'){
     if(Route::requestURI(3)){
-        $set='set';$setAdminCook='win'.Data::DatePass();
+        $setAdminCook='win'.Data::DatePass();$set='set';
         switch(Route::$uri_parts[0]){
             case $setAdminCook:
                 $setAdminCook=new \lib\user\User();$setAdminCook->setCookieAdmin();Route::$index=1;break;
-            //case $set:include'../modul/vt/admin/main.php';break;
-            //case 'fish':new \incl\vt\Rmenu\FishArticle();break;
+            case $set:include'../modul/win/admin/main.php';break;
+
             default:new \incl\win\Def\DefContent();
         }
     }
