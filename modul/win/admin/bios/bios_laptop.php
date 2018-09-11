38d6b7c9ccc3af4d031719b2a51f4a450743dfb3
Дом <admin@dp-malish.com>
2018-09-11 04:02:39 +0300

    win2 v0.3.2.10 скрипт manufacturer

--- modul/win/admin/bios/bios_laptop.php
+++ modul/win/admin/bios/bios_laptop.php
@@ -12,20 +12,56 @@ $manufactur=new Manufactur\Manufacturer();
 
 $res=$manufactur->getManufacturerDB();
 
-
+$man_select='<select onchange="changeManufacturBiosLaptop()">';
 foreach($res as $k=>$v){
-    Opt::$main_content.='<div class="fon_c" data-man_id="' . $v['manufacturer_id'] . '" data-man_name="' . $v['name'] . '" data-man_laptop="'.$v['laptop'].'">
-   
-    <div class="manufacturer_name">'.$v['name'].'</div>
-     
-    <div class="'.($v['laptop']?'on':'off').' manufacturer_laptop" onClick="onManufacturer(this)">ноут</div>
-    
-    <div class="cl"></div>
-    </div>';
+
+    $man_select.='<option value="'.$v['manufacturer_id'].'">'.$v['name'].'</option>';
+
+
+    $manufacturer_arr[$v['manufacturer_id']]=$v['name'];
+}
+$man_select.='<select>';
+
+Opt::$main_content.='<div class="fon_c">
+<div id="bios_laptop_manufacturer" class="static_data" data-bios_laptop_manufacturer="">'.$man_select.'</div>
+<div id="bios_laptop_link" class="static_data" data-bios_laptop_link="">link</div>
+<div id="bios_laptop_model" class="static_data" data-bios_laptop_model="" onclick="modelBiosLaptop()">model</div>
+<div id="bios_laptop_save" class="static_data" data-bios_laptop_save="" onclick="submitBiosLaptop()">Добавить</div>
+<div class="cl"></div></div>
+<script type="application/javascript">
+
+function submitBiosLaptop(){
+  alert("submitBiosLaptop");
 }
 
+function modelBiosLaptop(){
+  alert("modelBiosLaptop");
+}
+
+function changeManufacturBiosLaptop(){
+  alert("changeManufacturBiosLaptop");
+}
+
+</script>
+';
 
 
+
+$res=SQListatic::arrSQL_('SELECT * FROM bios_laptop ORDER BY id DESC');
+
+foreach($res as $k=>$v){
+    Opt::$main_content.='<div class="fon_c" data-bios_lap_id="'.$v['id'].'">';
+
+
+    Opt::$main_content.='<div class="'.($v['status']?'on':'off').'" onClick="">'.($v['status']?'on':'off').'</div>';
+    Opt::$main_content.='<div class="static_data">'.$v['id'].'</div>';
+    Opt::$main_content.='<div class="static_data">'.$manufacturer_arr[$v['manufacturer_id']].'</div>';
+    Opt::$main_content.='<div class="static_data">'.$v['link'].'</div>';
+    Opt::$main_content.='<div class="static_data">'.$v['model'].'</div>';
+
+
+    Opt::$main_content.='<div class="cl"></div></div>';
+}
 
 
 
