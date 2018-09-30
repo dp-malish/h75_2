25f4c6167af59834d6b32777675f4c4c1e36aa26
Дом <admin@dp-malish.com>
2018-10-01 02:15:38 +0300

    win2 v0.3.2.11 скрипт bios laptop sota v0.0.0.1 main_menu

--- blocks/sota/common/header.php
+++ blocks/sota/common/header.php
@@ -1,28 +1,18 @@
-<body>
-<header>
+<body><header>
     <div class="header rel">
         <div class="headertop rel" id="getsizehed">
             <div id="headcontact" class="maxw five_ headerimg">
-                <div id="logo">
-                    <a href="/"><img src="/img/site/dbpic.php?i=logo&ep=1"></a>
-                </div>
-
-                <div id="logoname" class="h_field ac">
-                    <h1>Сервисный центр «Cота плюс»</h1>
-                </div>
-
+                <div id="logo"><a href="/"><img src="/img/site/dbpic.php?i=logo&ep=1"></a></div>
+                <div id="logoname" class="h_field ac"><h1>Сервисный центр «Cота плюс»</h1></div>
                 <div id="topcontact" class="h_field">
-
                     <div class="dwf">
-
                         <div id="clock">
                             <div class="fl clock mt"></div>
                             <div class="fr five_">
                                 <div class="ac"><span class="grey">пн-пт:</span> 10:00-18:00</div>
                                 <div class="ac"><span class="grey">сб:</span> 10:00-14:00</div>
                                 <div class="ac"><span class="grey">вс:</span> выходной</div>
-                            </div>
-                            <div class="cl"></div>
+                            </div><div class="cl"></div>
                         </div>
                         <div>
                             <div class="fl tel mt"></div>
@@ -30,8 +20,7 @@
                                 <div class="ac mb">Наши контакты:</div>
                                 <div><span class="grey telcountry">+38</span>(097) 523-77-87</div>
                                 <div><span class="grey telcountry">+38</span>(066) 911-84-44</div>
-                            </div>
-                            <div class="cl"></div>
+                            </div><div class="cl"></div>
                         </div>
                         <div id="adres">
                             <div class="fl adres mt"></div>
@@ -39,43 +28,24 @@
                                 <div class="ac mb">Наш адрес:</div>
                                 <div class="ac">г. Мариуполь</div>
                                 <div>пр-т Строителей, 39</div>
-                            </div>
-                            <div class="cl"></div>
+                            </div><div class="cl"></div>
                         </div>
                     </div>
-                </div>
-
-                <div class="cl"></div>
+                </div><div class="cl"></div>
             </div>
-
         </div>
-
-
-
         <div class="headercen rel">
             <div class="maxw rel">
                 <div id="main_slider" class="rel">
-
-
                     <img src="/img/site/slider/dbpic.php?i=servesniy_centor&ep=0" alt="Сервисный центр Мариуполь">
                     <img src="/img/site/slider/dbpic.php?i=site&ep=0" alt="Сделать сайт в Мариуполе">
                     <img src="/img/site/slider/dbpic.php?i=servesniy_centor&ep=0" alt="Сервисный центр Мариуполь">
                     <img src="/img/site/slider/dbpic.php?i=site&ep=0" alt="Сделать сайт в Мариуполе">
-
                     <div></div>
                     <span id="btnSlider"><button type="button" value="0">&nbsp;</button><button type="button" value="1">&nbsp;</button><button type="button" value="2">&nbsp;</button><button type="button" value="3">&nbsp;</button></span>
                 </div>
             </div>
         </div>
-
-
-
-        <div class="headerbot maxw rel">
-            <?=$burger;?>
-
-
-
-
-        </div>
+        <div class="headerbot maxw rel"><?=$burger;?></div>
     </div>
 </header>
