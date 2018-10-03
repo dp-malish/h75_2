53ff8056c8475110b2017bc864c6913f2058cc5d
Дом <admin@dp-malish.com>
2018-10-03 23:57:01 +0300

    win2 v0.3.2.11 скрипт bios laptop sota v0.0.0.3 login

---
+++ sql/user.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS user(
+  `user_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(96) NULL,
+  `tel` INT NULL,
+  `tel_2` INT NULL,
+  `user_group_user_group_id` INT NOT NULL,
+  `username` VARCHAR(20) NULL,
+  `password` VARCHAR(40) NULL,
+  `salt` VARCHAR(9) NULL,
+  `firstname` VARCHAR(32) NULL,
+  `lastname` VARCHAR(32) NULL,
+  `image` VARCHAR(255) NULL,
+  `code` VARCHAR(40) NULL,
+  `ip` VARCHAR(40) NULL,
+  `status` TINYINT(1) DEFAULT 1,
+  `date_added` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `tel` (`tel` ASC),
+  UNIQUE INDEX `email` (`email` ASC))
+  ENGINE = MyISAM
+  AUTO_INCREMENT = 1
+  DEFAULT CHARACTER SET = utf8
