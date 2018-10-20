6d1be497eec63973b7b48da82820670afa0217ce
Аптека <admin@dp-malish.com>
2018-10-20 13:20:23 +0300

    Win v 0.1 invoice class pdf

--- www.sota.my/ajax/site/login.php
+++ www.sota.my/ajax/site/login.php
@@ -3,29 +3,38 @@ namespace lib\Post;
 Error_Reporting(E_ALL & ~E_NOTICE);ini_set('display_errors',1);
 set_include_path(get_include_path().PATH_SEPARATOR.'../../../');spl_autoload_register();
 
-use lib\Def as Def;
+use lib\Def;
+use lib\User\UserRole;
 
 if(Post::issetPostArr()){
     $user=new \lib\User\UserRole(['../../../cache_all/sota'],false);
-    if(!empty($_POST['login'])){
+    if(!empty($_POST['login'])){//вход на сайт
         if($user->loginUserWithRole('mail','pass')){
             echo json_encode(['err'=>false,'answer'=>$user->answer]);
         }else{Post::answerErrJson();}
-    }elseif(!empty($_POST['info_user'])){
+    }elseif(!empty($_POST['info_user'])){//вся инфа про извера
         $user->getRoleUser();
         if(Def\Opt::$live_user!=0 && Def\Opt::$live_user!=6){
             if(!empty($_POST['id'])){
+                $id=Def\Validator::html_cod($_POST['id']);
+                if(Def\Validator::paternInt($id)){
+                    if($user->getUserInfo($id)){
+                        echo json_encode([
+                            'err'=>false,
+                            'id'=>$user->answer_arr['user_id'],
+                            'fio'=>$user->answer_arr['lastname'].'<br>'.$user->answer_arr['firstname'].'<br>'.$user->answer_arr['patronymic'],
+                            'tel'=>$user->answer_arr['tel'],
+                            'tel2'=>$user->answer_arr['tel2'],
+                            'mail'=>$user->answer_arr['email'],
+                            'role'=>UserRole::$user_role_arr[($user->answer_arr['user_group_id']-1)]['name'],
 
-                ///////////////////1111111!!!!!!
-
-                //$id=Def\Validator::html_cod();
-
-                $user->getUserInfo(2);
-
-                echo json_encode(['err'=>false,'id'=>$user->answer_arr['user_id'],'mail'=>$user->answer_arr['email']]);
-            }
+                            'data'=>$user->answer_arr['date_added'],
 
 
+                        ]);
+                    }
+                }else{Def\Validator::$ErrorForm[]='0x0000001';Post::answerErrJson();}
+            }else{Def\Validator::$ErrorForm[]='0x0000002';Post::answerErrJson();}
         }else{Def\Validator::$ErrorForm[]='Вход не выполнен!';Post::answerErrJson();}
     }else echo $_SERVER['SERVER_NAME'];
     //-------------------------------------------------------------------
--- www.sota.my/js/user_role.js
+++ www.sota.my/js/user_role.js
@@ -1,38 +1,8 @@
 function getUserInfo(id){
 
+    modalload(loaderImg);
 
     ajaxPostSend("info_user=1&id="+id,callbackGetUserInfo,true,true,"/ajax/site/login.php");
-    //alert(id);
-/*    var formLogin=document.createElement("form");
-    formLogin.id = "formLoginUser";
-    formLogin.className = "form";
-    formLogin.innerHTML="<h4>Вход</h4>";
-
-    var mailLogin = document.createElement("input");
-    mailLogin.id="btnUserEmail";
-    mailLogin.type = "email";
-    mailLogin.size = "95";
-    mailLogin.title = "Email для входа";
-    mailLogin.placeholder = "login@gmail.com";
-    mailLogin.setAttribute("required","");
-    formLogin.appendChild(mailLogin);
-
-    mailLogin = document.createElement("input");
-    mailLogin.id="btnUserPass";
-    mailLogin.type = "password";
-    mailLogin.size = "40";
-    mailLogin.title = "Пароль для входа";
-    mailLogin.placeholder = "Пароль для входа";
-    mailLogin.setAttribute("required","");
-    formLogin.appendChild(mailLogin);
-
-    mailLogin = document.createElement("input");
-    mailLogin.id="btnUserLogin";
-    mailLogin.type = "submit";
-    mailLogin.value = "Войти";
-    mailLogin.addEventListener("click",userLogin);
-    formLogin.appendChild(mailLogin);*/
-
 
 }
 
@@ -52,13 +22,142 @@ function getUserInfo(id){
     }
 }*/
 function callbackGetUserInfo(arr){
-    alert(arr.id+"   "+arr.mail);
+    modalloadclose();
 
-/*    if(arr.answer==1){
-        //modalloadclose();
-        //document.getElementById("login_btn").remove();
+    var canvas=document.createElement("div");
+    //группа id
+    var d=document.createElement("div");
 
-    }else */
-//modalloadFormAnswer("<p>"+arr.answer+"</p>");
+    var d_left=document.createElement("div");
+    d_left.setAttribute("class","ar ff fl");
+    d_left.innerHTML="№";
+    d.appendChild(d_left);
+    var d_right=document.createElement("div");
+    d_right.setAttribute("class","al ff fr");
+    d_right.innerHTML=arr.id;
+    d.appendChild(d_right);
+    var cl=document.createElement("div");
+    cl.setAttribute("class","cl");
+    d.appendChild(cl);
+    canvas.appendChild(d);
+    callbackGetUserInfo.id=arr.id;
+
+    //группа ФИО
+    d_fio=document.createElement("div");
+
+    d_left=document.createElement("div");
+    d_left.setAttribute("class","ar ff fl");
+    d_left.innerHTML="ФИО";
+    d_fio.appendChild(d_left);
+    d_right=document.createElement("div");
+    d_right.setAttribute("class","al ff fr");
+    d_right.innerHTML=arr.fio;
+    d_fio.appendChild(d_right);
+    cl=document.createElement("div");
+    cl.setAttribute("class","cl");
+    d_fio.appendChild(cl);
+
+    //крайняя див который всё содержит
+    canvas.appendChild(d_fio);
+    //callbackGetUserInfo.id=arr.id;
+
+    //группа telephone
+    d_tel=document.createElement("div");
+
+    d_left=document.createElement("div");
+    d_left.setAttribute("class","ar ff fl");
+    d_left.innerHTML="Телефон";
+    d_tel.appendChild(d_left);
+    d_right=document.createElement("div");
+    d_right.setAttribute("class","al ff fr");
+    d_right.innerHTML=arr.tel;
+    d_tel.appendChild(d_right);
+    cl=document.createElement("div");
+    cl.setAttribute("class","cl");
+    d_tel.appendChild(cl);
+
+    //крайняя див который всё содержит
+    canvas.appendChild(d_tel);
+
+    //группа telephone 2
+    d_tel2=document.createElement("div");
+
+    d_left=document.createElement("div");
+    d_left.setAttribute("class","ar ff fl");
+    d_left.innerHTML="Телефон";
+    d_tel2.appendChild(d_left);
+    d_right=document.createElement("div");
+    d_right.setAttribute("class","al ff fr");
+    d_right.innerHTML=arr.tel2;
+    d_tel2.appendChild(d_right);
+    cl=document.createElement("div");
+    cl.setAttribute("class","cl");
+    d_tel2.appendChild(cl);
+
+    //крайняя див который всё содержит
+    canvas.appendChild(d_tel2);
+
+
+    //группа email
+    d_email=document.createElement("div");
+
+    d_left=document.createElement("div");
+    d_left.setAttribute("class","ar ff fl");
+    d_left.innerHTML="Email";
+    d_email.appendChild(d_left);
+    d_right=document.createElement("div");
+    d_right.setAttribute("class","al ff fr");
+    d_right.innerHTML=arr.mail;
+    d_email.appendChild(d_right);
+    cl=document.createElement("div");
+    cl.setAttribute("class","cl");
+    d_email.appendChild(cl);
+
+    //крайняя див который всё содержит
+    canvas.appendChild(d_email);
+
+
+    //группа role
+    d_role=document.createElement("div");
+
+    d_left=document.createElement("div");
+    d_left.setAttribute("class","ar ff fl");
+    d_left.innerHTML="Доступ";
+    d_role.appendChild(d_left);
+    d_right=document.createElement("div");
+    d_right.setAttribute("class","al ff fr");
+    d_right.innerHTML=arr.role;
+    d_role.appendChild(d_right);
+    cl=document.createElement("div");
+    cl.setAttribute("class","cl");
+    d_role.appendChild(cl);
+
+    //крайняя див который всё содержит
+    canvas.appendChild(d_role);
+
+
+    //группа дата регистрации
+    d_reg=document.createElement("div");
+
+    d_left=document.createElement("div");
+    d_left.setAttribute("class","ar ff fl");
+    d_left.innerHTML="Дата регистрации";
+    d_reg.appendChild(d_left);
+    d_right=document.createElement("div");
+    d_right.setAttribute("class","al ff fr");
+    d_right.innerHTML=arr.data;
+    d_reg.appendChild(d_right);
+    cl=document.createElement("div");
+    cl.setAttribute("class","cl");
+    d_reg.appendChild(cl);
+
+    //крайняя див который всё содержит
+    canvas.appendChild(d_reg);
+
+
+    //"<p>"+arr.id+"   "+arr.mail+"</p>"
+    modalloadForm(null,canvas);
+
+    alert(callbackGetUserInfo.id)
 
 }
