a7f6d622433995db6ecaebd6f0e8916c4d73133f
Дом <admin@dp-malish.com>
2019-05-07 21:06:44 +0300

    Stroy 1.0

---
+++ incl/stroy/menu/defmenu.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Дом
+ * Date: 07.05.2019
+ * Time: 20:55
+ * menu 1 = верхнее меню (о-нас)
+ */
+
+namespace incl\stroy\Menu;
+use lib\Def as Def;
+
+class DefMenu{
+    /**
+     * DefMenu constructor.
+     * @param $index_menu integer
+     */
+    function __construct($index_menu){
+        switch($index_menu){
+            case 4:$this->menu_4_work_town();
+        }
+
+
+
+    }
+
+
+    private function menu_4_work_town(){
+
+    }
+
+
+
+
+
+
+}
