92ee13233e1505daaa9c9abd32a76c8a702f50fe
Александр <admin@dp-malish.com>
2019-05-10 16:40:38 +0300

    menu stroy + geography 1.0 + sitemap

--- modul/stroy/admin/common/sitemap.php
+++ modul/stroy/admin/common/sitemap.php
@@ -4,6 +4,7 @@
 <li><a href="/ajax/sitemap/?view" target="_blank">Общая карта сайта</a></li>
 <li><a href="/ajax/sitemap/def.php?view" target="_blank">Основной контент</a></li>
 <li><a href="/ajax/sitemap/l-menu-flat.php?view" target="_blank">Левое меню (квартиры)</a></li>
+<li><a href="/ajax/sitemap/geography.php?view" target="_blank">Левое меню (География работ)</a></li>
 <li><a href="/ajax/sitemap/icon-menu.php?view" target="_blank">Меню иконок (правое)</a></li>
 </ul>
 </div>';
