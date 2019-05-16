193fe3a279e1a2ce2ee8f2699f412430ed22e385
Дом <admin@dp-malish.com>
2019-05-16 23:33:40 +0300

    Sota magaz 0.0.1

--- sql/magaz/product.sql
+++ sql/magaz/product.sql
@@ -1,14 +1,34 @@
-CREATE TABLE heading(--Заголовок
+--Заголовок
+
+CREATE TABLE IF NOT EXISTS heading(
 id int(11) NOT NULL AUTO_INCREMENT,
-heading_name varchar(255),
+heading_name varchar(255) NULL COMMENT 'Название заголовка перед категорией',
+PRIMARY KEY (id)
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--Поставщик
+
+CREATE TABLE IF NOT EXISTS provider(
+id int(11) NOT NULL AUTO_INCREMENT,
+provider_name varchar(255) NULL COMMENT 'Название поставщика',
+PRIMARY KEY (id)
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO provider VALUES(1,'Mobimag'),(2,'Microtron'),(3,'ItPlanet');
+
+--location Склад
+
+CREATE TABLE IF NOT EXISTS location(
+id int(11) NOT NULL AUTO_INCREMENT,
+location_name varchar(255) NULL COMMENT 'Название Склада',
 PRIMARY KEY (id)
 )ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
+--номенклатура
 
-
-CREATE TABLE `product` (--номенклатура
-  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS nomenclature (
+  nomenclature_id int(11) NOT NULL AUTO_INCREMENT,
 
   heading TINYINT NOT NULL COMMENT 'Заголовок TINYINT',
   category TINYINT NULL COMMENT 'Категория TINYINT',
@@ -17,8 +37,9 @@ CREATE TABLE `product` (--номенклатура
   title varchar(255) NOT NULL,
   meta_d varchar(255) NOT NULL,
   caption varchar(255) NOT NULL,
+  short_text text,
 
-  `model` varchar(64) NULL COMMENT 'Модель',
+  `model` varchar(255) NULL COMMENT 'Модель',
   `model_short` varchar(64) NULL COMMENT 'Модель коротко',
 
   `sku` varchar(64) NULL COMMENT 'идентификатор товарной позиции - номенклатурная позиция',
@@ -26,18 +47,18 @@ CREATE TABLE `product` (--номенклатура
   `ean` varchar(14) NULL COMMENT 'универсальный код товара Европа',
   `isbn` varchar(17) NULL COMMENT 'универсальный код товара кодирования книг',
   `mpn` varchar(64) NULL COMMENT 'Manufacturer Part Number штрихкоды',
-  `location` varchar(128) NULL COMMENT 'место нахождения - склад',
+
+  `location` int (11) NULL COMMENT 'место нахождения - склад',
 
   `stock_status_id` int(11) NULL COMMENT 'идентификатор состояния акций',
 
   `image` varchar(255) NULL,
   `manufacturer_id` int(11) NOT NULL DEFAULT '1' COMMENT 'Производитель',
 
-
   /*`price` decimal(15,2) NOT NULL DEFAULT '0.00',Цена*/
+  markup smallint NULL COMMENT 'Наценка в %',
 
-
-  `date_available` date NULL COMMENT 'Дата поступления товара ожидается',
+  /*`date_available` date NULL COMMENT 'Дата поступления товара ожидается',*/
 
   `weight` decimal(15,3) NOT NULL DEFAULT '0.000' COMMENT 'Вес',
   `length` decimal(15,3) NOT NULL DEFAULT '0.000' COMMENT 'Длина',
@@ -46,14 +67,24 @@ CREATE TABLE `product` (--номенклатура
 
 
   `sort_order` int(11) NULL,
-  `status` tinyint(1) NULL,
+  `status` tinyint(1) NULL COMMENT 'Показывать',
+
   `viewed` int(5) NOT NULL DEFAULT '13' COMMENT 'Количество просмотров',
   `date_added` datetime NOT NULL COMMENT 'Дата доавления товара',
-  `date_modified` datetime NULL COMMENT 'Дата изменения товара',
+  /*`date_modified` datetime NULL COMMENT 'Дата изменения товара',*/
   /*`noindex` tinyint(1) NOT NULL DEFAULT '1',*/
-  PRIMARY KEY (`product_id`),
+  PRIMARY KEY (nomenclature_id),
   KEY (heading,category)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+INSERT INTO nomenclature(nomenclature_id, `heading`, `category`, `link`, `title`, `meta_d`,caption, `short_text`, `model`, `model_short`, `sku`, `upc`, `ean`, `isbn`, `mpn`, `location`, `stock_status_id`, `image`, `manufacturer_id`, `markup`, `weight`, `length`, `width`, `height`, `sort_order`, `status`, `viewed`, `date_added`) VALUES
+(1, 1, NULL, 'zaryadka', 'Зарядка', 'Зарядка meta_d','Заголовок caption' ,'Короткое описание зарядки с текстом коротким такая хрень.', 'Твердотельный накопитель 240Gb, PNY CS900, SATA3', 'SSD7CS900-240-PB', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '1,2,3', 1, 15, '0.000', '0.000', '0.000', '0.000', NULL, NULL, 13, '2019-05-16 00:00:00');
+
+
+
+
+
 
 
 
@@ -62,6 +93,9 @@ CREATE TABLE `product_2` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
   serial_number varchar(64) NULL COMMENT 'Серийный номер',
+  provider int(4) NULL COMMENT 'Поставщик',
+  kod_tovara_postavshika int(11) NULL,
+
 
   price_usd decimal(15,2) NOT NULL DEFAULT '0.00',
 
