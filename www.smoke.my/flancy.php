41569ab44c68bba9d133dacaa69f11cb97e23a58
Дом <admin@dp-malish.com>
2019-10-04 01:07:39 +0300

    Smoke Start 0.5.8 release sitemap

---
+++ www.smoke.my/flancy.php
@@ -0,0 +1,67 @@
+
+<!doctype html><html lang="ru"><head><link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon"><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="author" content="Александр Баранов"><meta name="copyright" lang="ru" content="MobileSmoke"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="robots" content="index,follow"><meta name="yandex-verification" content="cf7d12516e8b8b40"><meta name="google-site-verification" content="7bu1AuEffhwna4HjDMKrUwEOWW0KacyonJlt9VzYnKE"><meta name='wmail-verification' content='90961d9a291b4cba4672fe46efae4915'><script async src="/js/slider.php"></script><script async src="/js/scroll.php"></script><script async src="/js/common.php"></script><script async src="/js/call_back.php"></script><link rel="stylesheet" type="text/css" href="/css.php?v"><meta name="description" content="Большое количество наших клиентов смогли по достоинству оценить высокое качество и удобство горизонтальных кальянов MobileSmoke. Неповторимый густой и ароматный дым в сочетании с лёгкой тягой, сделают Ваш вечер оригинальным."><title>Плоские кальяны MobileSmoke - купить горизонтальный кальян MobileSmoke в Украине</title>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+</head><body><header>
+    <div class="header rel">
+        <div class="headertop rel">
+            <div id="headcontact" class="maxw topcontact">
+                <div class="h_field">
+                    <div class="dwf">
+                        <div id="clock">
+                            <div class="fl clock mt"></div>
+                            <div class="fr five_">
+                                <div class="ac"><span class="grey">пн-пт:</span> 10:00-18:00</div><div class="ac"><span class="grey">сб:</span> 10:00-14:00</div><div class="ac"><span class="grey">вс:</span> выходной</div>
+                            </div><div class="cl"></div>
+                        </div>
+                        <div id="tel">
+                            <div class="fl tel mt"></div>
+                            <div class="fr five_">
+                                <div class="ac mb">Наши контакты:</div>
+                                <div id="tel_k_pc_link"><a href="tel:+380684655519"><span class="grey telcountry">+38</span>(068) 465-55-19</a></div>
+                                <div id="tel_v_pc_text"><span class="grey telcountry">+38</span>(050) 859-95-29</div>
+                                <div id="tel_v_pc_link"><a href="tel:+380508599529"><span class="grey telcountry">+38</span>(050) 859-95-29</a></div>
+                            </div><div class="cl"></div>
+                        </div>
+                        <div id="adres">
+                            <div class="fl adres mt"></div>
+                            <div class="fr five_">
+                                <div class="ac mb">Наш склад:</div><div class="ac">г. Мариуполь</div><div>пр-т Строителей, 39</div>
+                            </div><div class="cl"></div>
+                        </div>
+                    </div>
+                </div><div class="cl"></div>
+            </div>
+            <div class="maxw rel ">
+                <div id="logo"><a href="/" title="Mobile Smoke - кальяны в Украине"><img src="/img/site/dbpic.php?i=logo&ep=1" alt="Mobile Smoke - кальяны в Украине"></a></div>
+                <div id="order" class="h_field">
+                    <div id="fast_order">
+                        <div id="fast_order_btn" class="call_back ac"><span>Обратный звонок</span></div>
+                    </div>
+                    <div id="full_order">
+                        <!-- корзина-->
+                    </div><div class="cl"></div>
+                </div><div class="cl"></div>
+            </div><div class="cl"></div>
+        </div>
+    </div><div class="cl"></div>
+    <div class="five five_"></div>
+    <div class="headercen rel"><div class="maxw rel"><div id="main_slider" class="rel"><img src="/img/site/slider/dbpic.php?i=Hookah_white&ep=1" alt="Кальян в Украине"><img src="/img/site/slider/dbpic.php?i=Hookah_black&ep=1" alt="Кальян в Украине"><img src="/img/site/slider/dbpic.php?i=Hookah_green&ep=1" alt="Кальян в Украине"><img src="/img/site/slider/dbpic.php?i=Hookah&ep=1" alt="Кальян в Украине"><div></div><span id="btnSlider"><button type="button" value="0">&nbsp;</button><button type="button" value="1">&nbsp;</button><button type="button" value="2">&nbsp;</button><button type="button" value="3">&nbsp;</button></span></div></div></div></header><div class="maxw rel"><div class="l_col rel"><div class="cl"></div></div><div id="m_col" class="rel"><!--Пр кол--><div class="r_col"><div class="cl"></div></div><!--Центр кол--><div id="c_col" class="rel"><main>
+
+
+
+
+
+
+                <a data-fancybox="gallery" href="/images/1.jpg"><img src="/images/1.jpg"></a>
+                <a data-fancybox="gallery" href="/images/2.jpg"><img src="/images/3.jpg"></a>
+                <a data-fancybox="gallery" href="/images/4.jpg"><img src="/images/4.jpg"></a>
+                <a data-fancybox="gallery" href="/images/5.jpg"><img src="/images/5.jpg"></a>
+
+
+
+
+
+            </main><div class="cl"></div></div><!--Конец Центр кола--><div class="cl"></div></div><!--end m_col--><div class="cl"></div></div><div id="af"></div><footer><div id="foot" class="maxw"><div id="copy" class="ac gt">Copyright &copy; smoke.my<br>2015-2019<br><br>Использование материалов сайта без разрешения правообладателя запрещено</div><div class="cl"></div></div><div id="bf"><div id="up"> ^ Наверх</div></div><div id="pulseBtn" class="pulse_btn"><div><div class="pulse_btn_phone"><i class="fa fa-phone" aria-hidden="true"></i>Тел</div><div class="pulse_btn_text">Кнопка связи</div></div></div></footer><link rel="stylesheet" type="text/css" href="/css/fontawesome.php"></body></html>
