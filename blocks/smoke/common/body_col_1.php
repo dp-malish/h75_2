cb54f212cdd70f7065fcde7744dbf9b7dc08ad33
Дом <admin@dp-malish.com>
2019-10-10 00:16:36 +0300

    Smoke Start 0.6.3 landing 0.5

--- blocks/smoke/common/body_col_1.php
+++ blocks/smoke/common/body_col_1.php
@@ -63,7 +63,7 @@
                     720 грн  <span class="land_price_old">850 грн</span>
                 </div>
                 <div>
-                <a class="land_btn_buy" href="#">Купить</a>
+                <a class="land_btn_buy" href="#land-fast-order">Купить</a>
                     <p>Наши покупатели успели по достоинству оценить высокое качество и удобство горизонтальных кальянов MobileSmoke. Невероятно густой и ароматный дым, легкая тяга, оригинальный дизайн, прочность и мобильность - лишь основные преимущества кальянов MobileSmoke. Вы и Ваши друзья останетесь довольны кальяном MobileSmoke вне зависимости от того, где Вы намерены его использовать.</p>
                     <ul class="item"><li><span class="check"></span>Плоский корпус MobileSmoke;</li><li><span class="check"></span>Силиконовая чаша;</li><li><span class="check"></span>Шланг и мундштук MobileSmoke;</li><li><span class="check"></span>Устройство контроля нагрева табака (Kaloud Lotus);</li><li><span class="check"></span>Подсветка с пультом дистанционного управления;</li><li><span class="check"></span>Металлические щипцы.</li></ul>
                 <div class="cl"></div>
@@ -124,15 +124,14 @@
             <li><span class="check"></span>В светодиодную шайбу вставляем батарейки (нужны 3 шт. батарейки ААА «мизинчиковые» - в комплект не входят);</li>
             <li><span class="check"></span>Устанавливаем светодиодную шайбу в корпус кальяна в специальное отверстие на дне кальяна;</li>
             <li><span class="check"></span>Заливаем в корпус воду через большое отверстие в верхней плоскости кальяна (по желанию добавляем лёд) по верхний уровень углубления светодиодной шайбы;</li>
-            <li><span class="check"></span></li>
-            <li><span class="check"></span></li>
-            <li><span class="check"></span></li>
-            <li><span class="check"></span></li>
-            <li><span class="check"></span></li>
-            <li><span class="check"></span></li>
-            <li><span class="check"></span></li>
+            <li><span class="check"></span>Во избежание залипания продувочного клапана рекомендуем обработать его перед использованием тальком / пудрой;</li>
+            <li><span class="check"></span>Устанавливаем силиконовый калауд;</li>
+            <li><span class="check"></span>Укладываем табак таким образом, чтобы табак не перекрывал отверстия в центре силиконовой чаши. Рекомендуем использовать легкий или средний табак - например Adalya или Sherbetli;</li>
+            <li><span class="check"></span>Разжигаем угли;</li>
+            <li><span class="check"></span>На силиконовую чашу устанавливаем калауд с углями и в течении 5 минут даем табаку прогреться;</li>
+            <li><span class="check"></span>Пока прогревается табак, устанавливаем шланг, после чего кальян можно использовать.</li>
+            <li><span class="check"></span>После использования кальян рекомендуем промыть теплой водой со средством для мытья посуды, после чего ополоснуть кальян и дать ему просохнуть высохнуть. Также рекомендуем промыть шланг и мундштук. После сушки убрать кальян в коробку до следующего использования.</li>
             </ul>
-
         </div>
     </div>
     <div class="cl"></div>
@@ -143,54 +142,44 @@
 
 <div class="rel">
     <div class="maxw">
-
-
-
-        re re er
-        rere
-
+        <div id="land-fast-order" class="five"></div>
+        <h3>Сделать заказать по акции:</h3>
+        <form class="land_form_fast_buy">
+            <div class="rel">
+                <input id="formFastFIOLand" name="username" type="text" size="33" title="Введите Ваше имя" placeholder="Фамилия и Имя *" required="">
+            </div>
+            <div class="rel">
+                <input id="formFastTelLand" name="tel" type="text" size="13" title="Введите номер телефона" placeholder="Номер телефона *" required="">
+            </div>
+            <div class="rel">
+                <input id="formFastTownLand" name="town" type="text" size="13" title="Введите Ваш город" placeholder="Город">
+            </div>
+            <div class="rel">
+                <input id="formFastNPostLand" name="n_post" type="text" size="4" title="Введите номер Новой почты" placeholder="Номер отделения новой почты">
+            </div>
+            <div class="rel">
+            <select class="rel" onchange="this.style.color='black'"><!--style="color:gray"-->
+                <option value="">Выбрать цвет</option><!-- style="display:none"-->
+                <option value="black">Чёрный</option>
+                <option value="white">Белый</option>
+                <option value="red">Красный</option>
+                <option value="green">Зелёный</option>
+                <option value="blue">Синий</option>
+            </select>
+            </div>
+            <div class="rel">
+                <input type="submit" value="Заказать">
+            </div>
+        </form>
     </div>
     <div class="cl"></div>
 </div>
 
 
 
-<div>
-    <div class="land_fon_c">
-        <div class="maxw">
-            re re er
-            rere
-        </div>
-    </div>
-    <div class="cl"></div>
-</div>
 
 
 
 
 
 
-тут колонки
-<div class="maxw rel"><div class="l_col rel"><div class="cl"></div></div><div id="m_col" class="rel"><!--Пр кол--><div class="r_col"><div class="cl"></div></div><!--Центр кол--><div id="c_col" class="rel"><main>
-
-
-
-
-
-
-
-
-                <!-- Add images to <div class="fotorama"></div> -->
-                <div class="fotorama" data-allowfullscreen="native" data-nav="thumbs" data-transition="crossfade" data-loop="true" data-autoplay="7000" data-keyboard="true" >
-                    <img src="https://s.fotorama.io/1.jpg">
-                    <img src="https://s.fotorama.io/2.jpg">
-                    <img src="/images/5.jpg">
-
-                    <img src="/images/4.jpg"><img src="/images/3.jpg"><img src="/images/1.jpg">
-                </div>
-
-
-
-
-
-            </main><div class="cl"></div></div><!--Конец Центр кола--><div class="cl"></div></div><!--end m_col--><div class="cl"></div></div>
