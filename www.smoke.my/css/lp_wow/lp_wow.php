588b7ead5aa95b4db4ed573c863dfc51e9bc2a2a
Александр <admin@dp-malish.com>
2019-12-11 16:32:16 +0200

    fb

---
+++ www.smoke.my/css/lp_wow/lp_wow.php
@@ -0,0 +1,6 @@
+<?php
+namespace lib\Css;
+Error_Reporting(E_ALL & ~E_NOTICE);ini_set('display_errors',1);
+set_include_path(get_include_path().PATH_SEPARATOR.'../../../');spl_autoload_register();
+$css=new Css(['..','..','..','cache_all','smoke','css'],false);
+$css->SendCssSelect('smoke/lp_wow','lp_wow',['pulse_btn_','fontawesome'],2);
