6d7ab7f3c808aaa4dcf7667da3064585ea7fc28d
Дом <admin@dp-malish.com>
2020-01-21 00:29:33 +0200

    Smoke Start 0.6.8 landing 1.0 win lng 0.1

---
+++ modul/smoke/lp_humidifier_2.php
@@ -0,0 +1,213 @@
+<?php
+namespace lib\Def;
+
+\incl\smoke\Def\OptSmoke::$body_column=NULL;
+
+Opt::$main_content.='<!DOCTYPE html><html lang="ru"><head><link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon"><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="robots" content="index,follow">
+<meta property="og:image" content="/img/humidifier/meta.jpg"><meta property="og:title" content="Humidifier - ультразвуковой увлажнитель воздуха"><meta property="og:description" content="Увлажнитель воздуха HUMIDIFIER – отличный выбор в сегменте портативных мини-увлажнителей воздуха и ароматизаторов с лаконичным дизайном и достойным функционалом."><title>Humidifier - ультразвуковой увлажнитель воздуха</title><meta name="description" content="Увлажнитель воздуха HUMIDIFIER – отличный выбор в сегменте портативных мини-увлажнителей воздуха и ароматизаторов с лаконичным дизайном и достойным функционалом.">
+<script async src="/js/common.php"></script><script async src="/js/call_back.php"></script>
+<link rel="stylesheet" href="/css/lp_humidifier/body.php"><!--<script src="/js/wow.php"></script><script>new WOW().init();</script>-->
+<script async src="/js/scroll_only.php"></script><script async src="/js/lp_humidifier.php"></script></head><body><!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=\'2.0\';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,\'script\',
+        \'https://connect.facebook.net/en_US/fbevents.js\');
+    fbq(\'init\', \'2534848886552351\');
+    fbq(\'track\', \'PageView\');
+    fbq(\'track\', \'ViewContent\');
+    fbq(\'track\', \'Purchase\');
+</script>
+<noscript>
+    <img height="1" width="1"
+         src="https://www.facebook.com/tr?id=2534848886552351&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
+<div class="min_h first_d rel">
+    <div class="rel">
+        <div class="maxw rel">
+            <div id="logo"><a href="/" title="SOpt Store - в Украине"><img src="/img/site/dbpic.php?i=logo&ep=1" alt="SOpt Store"></a></div>
+            <nav>
+                <div class="dexter_menu">
+                    <input type="checkbox" id="dexter_menu_check">
+                    <label for="dexter_menu_check"></label>
+                    <div class="dexter-main-menu-top">
+                        <span class="dexter_menu_label">MENU</span>
+                    </div>
+                    <div class="dexter-line first"></div><div class="dexter-line second"></div><div class="dexter-line third"></div><div class="dexter-line fourth"></div>
+                    <nav class="dexter-main-menu">
+                        <a href="/">Главная</a>
+                        <a href="#zakazat">Заказать</a>
+                        <a href="#preimushestva">Преимущества</a>
+                        <a href="#to-use">Инструкция</a>
+                        <a href="#primenenie">Применение</a>
+                    </nav>
+                </div>
+            </nav>
+            <div class="cl"></div>
+            <div class="rel top_content">
+                <h3 class="pulse">Humidifier - ультразвуковой увлажнитель воздуха</h3>
+                <div id="d_fon_top_img">
+                    <img src="/img/fast_order/humidifier_fon.png" alt="Humidifier - ультразвуковой увлажнитель воздуха" class="bounceInLeft">
+                    <div class="cl"></div>
+                </div>
+                <div id="d_fon_top_r" class="fr">
+                    <br><br><br><p class="bounceInRight">Оставьте заявку и мы с Вами свяжемся в ближайшее время, чтобы обсудить детали заказа в телефонном режиме. Или Вы можете продолжить покупки и оформить товар из корзины.</p>
+                    <div id="top_call_back_btn" class="ac bounceInUp">Перезвонить мне</div>
+                    <script>document.getElementById("top_call_back_btn").addEventListener("click",function(){formCalBackViewl();},false);</script>
+                </div><div class="cl"></div>
+            </div><div class="cl"></div>
+        </div>
+    </div><div class="cl"></div>
+</div>
+<!--вторая часть  fotorama.css нужен-->
+<div id="zakazat" class="min_h fon rel">
+    <div class="land_fon_c rel">
+        <div class="maxw rel">
+            <section>
+                <div class="land_fon_c_l rel">
+                    <div class="fotorama" data-allowfullscreen="native" data-nav="thumbs" data-transition="crossfade" data-loop="true" data-autoplay="7000" data-keyboard="true" >
+                        <img src="/img/humidifier/dbpic.php?i=humidifier-two" alt="Humidifier - пара"><img src="/img/humidifier/dbpic.php?i=humidifier-main" alt="Humidifier - презентация"><img src="/img/humidifier/dbpic.php?i=humidifier-avto" alt="Humidifier - авто"><img src="/img/humidifier/dbpic.php?i=humidifier-fon" alt="Humidifier - фон"><img src="/img/humidifier/dbpic.php?i=humidifier-blue" alt="Humidifier - голубой"><img src="/img/humidifier/dbpic.php?i=humidifier-pink" alt="Humidifier - розовый"><img src="/img/humidifier/dbpic.php?i=humidifier-white" alt="Humidifier - белый"><img src="/img/humidifier/dbpic.php?i=humidifier-home" alt="Humidifier - дом"><img src="/img/humidifier/dbpic.php?i=humidifier-office" alt="Humidifier - офис"><img src="/img/humidifier/dbpic.php?i=humidifier-packaging" alt="Humidifier - офис"><img src="/img/humidifier/dbpic.php?i=humidifier-details" alt="Humidifier - деталировка"><img src="/img/humidifier/dbpic.php?i=humidifier-econom" alt="Humidifier - эконом">
+                        <a href="https://youtu.be/2UCxDNoc2GA"></a>
+                    </div>
+                </div>
+                <div class="land_fon_c_r rel">
+                    <h3 class="pulse">Humidifier - ультразвуковой увлажнитель воздуха</h3>
+                    <div class="rel"></div>
+                    <div class="land_price bounceInRight">349 грн  <span class="land_price_old">560 грн</span></div>
+                    <div>
+                        <div id="land_btn_buy" class="land_btn_buy ac bounceInUp">Заказать</div><script>document.getElementById("land_btn_buy").addEventListener("click",function(){location.href="#order-buy";},false);</script>
+                        <p class="bounceInRight">Увлажнитель воздуха HUMIDIFIER – отличный выбор в сегменте портативных мини-увлажнителей воздуха и ароматизаторов с лаконичным дизайном и достойным функционалом.</p>
+                        <p>Увлажнитель Humidifier абсолютно безшумный, кабель USB позволяет подключить его в любом удобном для вас месте, а благодаря оригинальной LED подсветке (7 цветов), его можно использовать в комнате как ночник.</p>
+                        <h4>Технические характеристики</h4>
+                        <ul class="item bounceInLeft">
+                            <li><span class="check"></span>Обьем резервуара: 300мл;</li>
+                            <li><span class="check"></span>Скорость испарения жидкости: 30 мл.час;</li>
+                            <li><span class="check"></span>Мощность: 1.5-2Вт;</li>
+                            <li><span class="check"></span>Рабочее напряжение: 12V;</li>
+                            <li><span class="check"></span>Размер: 5,7*5,6*16,4см;</li>
+                            <li><span class="check"></span>Материал: АБС пластик, электронные компоненты;</li>
+                            <li><span class="check"></span>Шум: 36 Дб;</li>
+                            <li><span class="check"></span>Вес упаковки: 153 грамма;</li>
+                            <li><span class="check"></span>Цвет: белый, розовый, бирюзовый!</li>
+                        </ul>
+                        <div class="cl"></div>
+                    </div>
+                </div>
+            </section>
+        </div><div class="cl"></div>
+    </div><div class="cl"></div>
+</div><script src="/js/jq.php"></script><script src="/js/fotorama.php"></script>
+<!--третья часть-->
+<div id="preimushestva" class="min_h fon2">
+    <div class="maxw land_benefits">
+        <section>
+            <div class="ac land_benefits_caption">
+                <h3>Наши преимущества</h3>
+                <p>В нашей компании вы получаете не только 100% качественный товар, но и индивидуальный подход к каждому клиенту!</p>
+            </div>
+            <div class="land_benefits_border cl"><br></div>
+            <div class="dwfte">
+                <div class="land_benefits_box five rollIn">
+                    <span class="land_pay_i fl"></span>
+                    <span><h4>Способы оплаты</h4><p>При получении на почте или на карту ПриватБанка</p></span>
+                    <div class="cl"></div>
+                </div>
+                <div class="land_benefits_box five lightSpeedIn">
+                    <span class="land_quality_i fl"></span>
+                    <span><h4>Гарантия качества</h4><p>Возврат и обмен в течении 14 дней согласно закона</p></span>
+                    <div class="cl"></div>
+                </div>
+            </div>
+            <div class="land_benefits_border cl"></div>
+            <div class="dwfte">
+                <div class="land_benefits_box five rollIn">
+                    <span class="land_delivery_i fl"></span>
+                    <span><h4>Быстрая доставка</h4><p>Получите свой заказ в течении 1-2 дней с момента заказа</p></span>
+                    <div class="cl"></div>
+                </div>
+                <div class="land_benefits_box five lightSpeedIn">
+                    <span class="land_price_i fl"></span>
+                    <span><h4>Доступные цены</h4><p>Вы можете приобрести ультразвуковой увлажнитель воздуха всего за 349 грн</p></span>
+                    <div class="cl"></div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="cl five five_"></div>
+</div>
+<!--четвёртая часть-->
+<div id="to-use" class="min_h fon rel">
+    <div class="land_fon_c rel">
+        <div class="maxw rel">
+            <h3 class="ac">Простота использования</h3><br>
+            <ul class="item bounceInLeft">
+                <li><span class="check"></span>Перед тем как применять устройство, налить в резервуар чистой воды.</li>
+                <li><span class="check"></span>Подключить увлажнитель к прикуривателю.
+                <li><span class="check"></span>Если растворить в воде пару капель ароматического масла, то в добавок к увлажнению происходит освежение салона. Важно использовать только растворимые в воде масла.</li>
+                <li><span class="check"></span>Струи пара регулируются и направляются в нужную сторону.</li>
+                <li><span class="check"></span>Устройство стоит применять только в заведенном автомобиле.</li>
+                <li><span class="check"></span>Вода должна быть очищенной. Важно следить за тем, чтобы она не заканчивалась.</li>
+                <li><span class="check"></span>При соблюдении этих условий, увлажнитель воздуха повысит качество поездки и приятно освежит воздух в салоне.</li>
+            </ul>
+            <br>
+            <p class="bounceInUp">Если вы хотите, чтобы вас всегда сопровождал приятный аромат- то увлажнитель Humidifier для Вас!</p>
+        </div><div class="cl"></div>
+    </div><div class="cl"></div>
+</div>
+<!--пятая часть-->
+<div id="primenenie" class="min_h fon2">
+    <div class="maxw">
+        <h3 class="ac">Оптимальное решение для ВАС</h3><br>
+        <p class="bounceInLeft">Сухой воздух – довольно частая проблема для замкнутых помещений, будь то комната или салон автомобиля. Мало того, что им просто неприятно дышать, так он еще и неблаготворно сказывается на вашем здоровье, является причиной целого ряда заболеваний.</p>
+        <p class="bounceInRight">С этим небольшим и недорогим устройством ваши поездки станут намного комфортнее!</p>
+        <ul class="item bounceInLeft">
+            <li><span class="check"></span>Сохраняет увлажненность воздуха в салоне автомобиля</li>
+            <li><span class="check"></span>Применяется как аромалампа</li>
+            <li><span class="check"></span>В режиме освежителя снимает психологическое напряжение</li>
+            <li><span class="check"></span>Маленький размер и компактность обеспечивают удобство хранения</li>
+        </ul>
+        <p>Ультразвуковой увлажнитель Humidifier - просто необходим в каждом доме, особенно если в нём есть ребенок, он поможет создать оптимальный микроклимат и обеспечит необходимую влажность в помещении (особенно в отопительный сезон), благодаря чему улучшается сон малыша и взрослых, легко и быстро освежает, увлажняет кожу, создаёт микроклимат в вашем доме, автомобиле или в офисе. Благодаря своим компактным размерам, вы можете брать его с собой на работу, использовать в автомобиле при дальних поездках, как всем известно, духота в автомобиле ведет к потере внимания и апатичности, ведь недостатки систем вентиляции современных автомобилей в том, что они нарушают электрический баланс свежего воздуха, которым они снабжают салон, увлажнитель Humidifier с лёгкостью решит эту проблему.</p>
+    </div>
+    <div class="cl five five_"></div>
+</div>
+<!--шестая часть-->
+<div id="order-buy" class="min_h fon rel">
+    <div class="land_fon_c rel">
+        <div class="maxw rel">
+            <div id="land-fast-order" class="five"></div>
+            <section><h2 class="ac bounceInDown">Сделать заказать по акции:</h2>
+                <form id="land_form_fast_buy" class="land_form_fast_buy">
+                    <div class="rel">
+                        <input id="formFastFIOLand" name="username" type="text" size="33" title="Введите Ваше имя" placeholder="Фамилия и Имя *" required="">
+                    </div>
+                    <div class="rel">
+                        <input id="formFastTelLand" name="tel" type="text" size="13" title="Введите номер телефона" placeholder="Номер телефона *" required="">
+                    </div>
+                    <div class="rel">
+                        <input id="formFastTownLand" name="town" type="text" size="13" title="Введите Ваш город" placeholder="Город">
+                    </div>
+                    <div class="rel">
+                        <input id="formFastNPostLand" name="n_post" type="number" min="1" title="Введите номер Новой почты" placeholder="Номер отделения новой почты">
+                    </div>
+                    <div class="rel">
+                        <select id="formFastColorLand" class="rel" onchange="this.style.color=\'black\'">
+                            <option value="">Выбрать цвет</option>
+                            <option value="white-Humidifier">Белый</option>
+                            <option value="pink-Humidifier">Розовый</option>
+                            <option value="turquoise-Humidifier">Бирюзовый</option>
+                        </select>
+                    </div>
+                    <div class="rel"><input id="formFastBuySubmitLand" class="bounceInUp" type="submit" value="Заказать"></div>
+                </form>
+            </section>
+            </div><div class="cl"></div>
+        </div><div class="cl"></div>
+    </div><div class="cl"></div>
+</div>
+<div id="pulseBtn" class="pulse_btn"><div><div class="pulse_btn_phone"><i class="fa fa-phone" aria-hidden="true"></i><span class="grey"></span></div><div class="pulse_btn_text">Кнопка связи</div></div></div><link rel="stylesheet" href="/css/lp_humidifier/pulse_btn.php"><link href="/css/fotorama.php" rel="stylesheet"><div id="bf"><div id="up"> ^ Наверх</div></div></body></html>';
