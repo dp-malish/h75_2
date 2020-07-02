285faf3207e32f8db9bac7475f0577845d921569
Дом <admin@dp-malish.com>
2020-07-02 21:21:40 +0300

    Smoke Start 0.6.8 landing 1.0 win lng 0.1

--- incl/win/bas/freehost.php
+++ incl/win/bas/freehost.php
@@ -19,8 +19,19 @@ class FreeHost extends Post{
         if(self::issetPostKey(['mail','login','pass'])){
             $mail=Def\Validator::auditMail($_POST['mail']);
             if(!$mail){$err=true;}
-
-
+            $login=Def\Validator::auditText($_POST['login'],'login');
+            if(!$login){$err=true;}
+            $pass=Def\Validator::auditText($_POST['pass'],'pass');
+            if(!$pass){$err=true;}
+            if(!$err){//добавить в БД
+                $ip=Def\Validator::getIp();
+                $DB=new Def\SQLi();
+                $sql='INSERT INTO freewebhostingarea(mail,login,pass,ip)VALUES(?,?,?,?)';
+                $sql=$DB->realEscape($sql,[$mail,$login,$pass,$ip]);
+                if(!$DB->boolSQL($sql)){$err=true;
+                    Def\Validator::$ErrorForm[]='Ошибка соединения, повторите попытку позже...';
+                }
+            }
         }else{$err=true;}
         return($err)?false:true;
     }
--- sql/bas/freewebhostingarea.sql
+++ sql/bas/freewebhostingarea.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS freewebhostingarea(
   mail varchar(255) NOT NULL,
   login varchar(255) NOT NULL,
   pass varchar(255) NOT NULL,
+  ip varchar(255) NOT NULL,
   `data` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `data_proverki` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (id)
