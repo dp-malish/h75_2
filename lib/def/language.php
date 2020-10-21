8d550e56194130330ff08c0255994397c08b02ec
Дом <admin@dp-malish.com>
2020-10-21 20:13:50 +0300

    Smoke Start 0.6.8 landing 1.0 win lng 0.1 burger 0.0.0.4

--- lib/def/language.php
+++ lib/def/language.php
@@ -4,13 +4,23 @@ use lib\Get as Get;
 
 /*
 *Входной параметр для языка $_GET['lng']
-*
+*Входной параметр для языка cookie 'lng'
 */
 
 
 
 class Language{
-    function __construct($lngDB=''){
+    function __construct($get=true,$lngDB=''){
+
+        if($get){
+            $this->takeGetLng();
+
+        }else{
+            //e3w
+
+        }
+
+
         if($lngDB==''){
             $cookie=Validator::issetCookie('lng');
             if($cookie){//если есть куки языка
@@ -34,4 +44,21 @@ class Language{
             }else return false;
         }else return false;
     }
+    protected function takeCookieLng(){
+        $cookie=Validator::issetCookie('lng');
+        if($cookie){//если есть куки языка
+            Opt::$lang=$cookie;
+        }else{//если нет куков языка
+            $lng=substr(Validator::html_cod($_SERVER['HTTP_ACCEPT_LANGUAGE']), 0, 2);
+            if($lng!=''){
+                Opt::$lang=$lng;
+                Cookie::setCookie('lng',$lng);
+            }
+        }
+    }
+    static function setLanguageManually($lng='ru'){
+        Opt::$lang=$lng;
+        Cookie::setCookie('lng',$lng);
+    }
+
 }
