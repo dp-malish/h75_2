349bf94299990c6d555c8ef0abc5842f78d6a1e4
Александр <admin@dp-malish.com>
2020-10-27 16:08:14 +0200

    burger 0.0.0.3

--- incl/burger/index/indexcontent.php
+++ incl/burger/index/indexcontent.php
@@ -18,9 +18,13 @@ class IndexContent{
         'uk'=>'Бургерна Барін Бургер у Маріуполі. Доставка смачної піци та бургерів. Безкоштовна доставка по Маріуполю.'];
 
     private $ves=[
-        'ru'=>'гg',
+        'ru'=>'г',
         'uk'=>'г',
         ];
+    private $sm=[
+        'ru'=>'см',
+        'uk'=>'см',
+    ];
 
   function __construct(){
 
@@ -36,7 +40,15 @@ class IndexContent{
       if($res){//$v['category']
           foreach($res as $k=>$v){
               switch($v['category']){
-                  case 1:$burger_txt.=$this->unitTemplates($v['img'],$v['img_alt_'.Def\Opt::$lang],$v['img_title_'.Def\Opt::$lang],$v['id'],$v['price'],$v['hit'],$v['kind'],$this->ves[Def\Opt::$lang]);
+                  case 1:$burger_txt.=$this->unitTemplates($v['img'],$v['img_alt_'.Def\Opt::$lang],$v['img_title_'.Def\Opt::$lang],$v['id'],$v['price'],$v['hit'],$v['kind'],$this->ves[Def\Opt::$lang],$v['cap_'.Def\Opt::$lang],$v['short_text_'.Def\Opt::$lang]);
+                  break;
+
+                  case 3:
+                      $kind=json_decode($v['kind']);
+                      if(is_array($kind)){
+                      $nuggets_txt.=$this->unitTemplatesKind2($v['img'],$v['img_alt_'.Def\Opt::$lang],$v['img_title_'.Def\Opt::$lang],$v['id'],$v['price'],$v['hit'],$kind,$this->ves[Def\Opt::$lang],$v['cap_'.Def\Opt::$lang],$v['short_text_'.Def\Opt::$lang]);
+                      }else{$nuggets_txt.=$this->unitTemplates($v['img'],$v['img_alt_'.Def\Opt::$lang],$v['img_title_'.Def\Opt::$lang],$v['id'],$v['price'],$v['hit'],$v['kind'],$this->ves[Def\Opt::$lang],$v['cap_'.Def\Opt::$lang],$v['short_text_'.Def\Opt::$lang]);}
+                  break;
               }
           }
       }else{Def\Route::$module404=true;}
@@ -55,19 +67,57 @@ class IndexContent{
 
       Def\Opt::$main_content.='<div id="nuggets-section"><h2>'.$lng::$caption_nuggets[Def\Opt::$lang].'</h2></div><div class="tile dwfe maxw">'.$nuggets_txt.'</div>';
 
-      Def\Opt::$main_content.='<div id="beverages-section"><h2>'.$lng::$caption_beverages[Def\Opt::$lang].'</h2></div><br><br><br><br><br><br><br><br>';
+      Def\Opt::$main_content.='<div id="beverages-section"><h2>'.$lng::$caption_beverages[Def\Opt::$lang].'</h2></div><br><br><br><br><br><br><br><br>
+
+<script type="text/javascript">
+    function selectUnit(el){
+        //if(el.classList.contains("unit_kind_pizza_select")){} else{}
+            var papa=el.parentNode;
+            var arrDiv=papa.getElementsByTagName("div");
+            arrDiv[0].classList.remove("unit_kind_pizza_select");
+            arrDiv[1].classList.remove("unit_kind_pizza_select");
+            el.classList.add("unit_kind_pizza_select");
+            var id=papa.parentNode.dataset.id;
+            papa.parentNode.dataset.price=el.dataset.price;
+        document.getElementById("priceUnit"+id).innerHTML=el.dataset.price+" ₴";
+    }
+</script>
+
+
+';
   }
-  private function unitTemplates($img,$img_alt,$img_title,$id,$price,$hit,$kind,$kind_ext){
+
+
+  private function unitTemplates($img,$img_alt,$img_title,$id,$price,$hit,$kind,$kind_ext,$cap,$short_text){
       if($img!=''){$img='<img src="'.BurDef\SqlTable::getImgDirTable('food_img').$img.'" alt="'.$img_alt.'" title="'.$img_title.'">';}else{$img='';}
       $txt='<div class="unit" data-id="'.$id.'" data-price="'.$price.'"><div class="unit_img">'.$img.'</div>';
       if($hit){$txt.='<div class="unit_hit"></div>';}
       ///если бургер шаблон
       $txt.='<div class="unit_kind_bur">'.$kind.' '.$kind_ext.'</div>';
-      $txt.='<div class="unit_capt">'.$v['cap_'.Def\Opt::$lang].'</div>';
-      $txt.='<div class="unit_hint">'.$v['short_text_'.Def\Opt::$lang].'</div>';
-      $txt.='<div class="unit_price">'.$v['price'].' ₴</div>';
+      $txt.='<div class="unit_capt">'.$cap.'</div>';
+      $txt.='<div class="unit_hint">'.$short_text.'</div>';
+      $txt.='<div class="unit_price">'.$price.' ₴</div>';
       $txt.='</div>';
       return $txt;
   }
+
+//вид как у пиццы
+  private function unitTemplatesKind2($img,$img_alt,$img_title,$id,$price,$hit,$kind,$kind_ext,$cap,$short_text){
+        if($img!=''){$img='<img src="'.BurDef\SqlTable::getImgDirTable('food_img').$img.'" alt="'.$img_alt.'" title="'.$img_title.'">';}else{$img='';}
+        $arr_kind_count=count($kind);
+        $price=json_decode($price);
+        $kind_html='';
+        for($i=0;$i<$arr_kind_count;$i++){
+            $kind_html.='<div class="unit_kind_pizza'.($i==0?' unit_kind_pizza_select':'').'" onclick="selectUnit(this);" data-price="'.$price[$i].'">'.$kind[$i].'</div>';
+        }
+        $txt='<div class="unit" data-id="'.$id.'" data-price="'.$price[0].'"><div class="unit_img">'.$img.'</div>';
+        if($hit){$txt.='<div class="unit_hit"></div>';}
+        $txt.='<div class="unit_kind"><span>'.$kind_ext.'</span>'.$kind_html.'</div>';;
+        $txt.='<div class="unit_capt">'.$cap.'</div>';
+        $txt.='<div class="unit_hint">'.$short_text.'</div>';
+        $txt.='<div id="priceUnit'.$id.'" class="unit_price">'.$price[0].' ₴</div>';
+        $txt.='</div>';
+        return $txt;
+    }
 
 }
