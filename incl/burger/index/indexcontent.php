b77804b9b390c3c432270b51a706a643e681a1ef
Александр <admin@dp-malish.com>
2020-10-28 13:34:51 +0200

    burger 0.0.0.3

--- incl/burger/index/indexcontent.php
+++ incl/burger/index/indexcontent.php
@@ -17,44 +17,41 @@ class IndexContent{
         'ru'=>'Бургерная Барин Бургер в Мариуполе. Доставка вкусной пиццы и бургеров. Бесплатная доставка по Мариуполю.',
         'uk'=>'Бургерна Барін Бургер у Маріуполі. Доставка смачної піци та бургерів. Безкоштовна доставка по Маріуполю.'];
 
-    private $ves=[
-        'ru'=>'г',
-        'uk'=>'г',
-        ];
-    private $sm=[
-        'ru'=>'см',
-        'uk'=>'см',
-    ];
+    private $ves=['ru'=>'г','uk'=>'г'];
+    private $vesH2O=['ru'=>'мл','uk'=>'мл'];
+    private $sm=['ru'=>'см','uk'=>'см'];
+    private $cool_drink=['ru'=>'Холодные напитки','uk'=>'Холодні напої'];
+    private $hot_drink=['ru'=>'Горячие напитки','uk'=>'Гарячі напої'];
 
   function __construct(){
-
       $lng=new BurDef\Template();
-
       $DB=new Def\SQLi();
       $res=$DB->arrSQL('SELECT id,category,link_turn,cap_'.Def\Opt::$lang.',img,img_alt_'.Def\Opt::$lang.',
       img_title_'.Def\Opt::$lang.',short_text_'.Def\Opt::$lang.',kind,price,hit FROM food ORDER BY category');
-
       $burger_txt='';
       $pizza_txt='';
       $nuggets_txt='';
-
+      $cool_drink_txt='';
+      $hot_drink_txt='';
       if($res){//$v['category']
           foreach($res as $k=>$v){
               switch($v['category']){
                   case 1:$burger_txt.=$this->unitTemplates($v['img'],$v['img_alt_'.Def\Opt::$lang],$v['img_title_'.Def\Opt::$lang],$v['id'],$v['price'],$v['hit'],$v['kind'],$this->ves[Def\Opt::$lang],$v['cap_'.Def\Opt::$lang],$v['short_text_'.Def\Opt::$lang]);
                   break;
-
                   case 2:
                       $kind=json_decode($v['kind']);
                       $pizza_txt.=$this->unitTemplatesKind2($v['img'],$v['img_alt_'.Def\Opt::$lang],$v['img_title_'.Def\Opt::$lang],$v['id'],$v['price'],$v['hit'],$kind,$this->sm[Def\Opt::$lang],$v['cap_'.Def\Opt::$lang],$v['short_text_'.Def\Opt::$lang]);
                   break;
-
                   case 3:
                       $kind=json_decode($v['kind']);
                       if(is_array($kind)){
                       $nuggets_txt.=$this->unitTemplatesKind2($v['img'],$v['img_alt_'.Def\Opt::$lang],$v['img_title_'.Def\Opt::$lang],$v['id'],$v['price'],$v['hit'],$kind,$this->ves[Def\Opt::$lang],$v['cap_'.Def\Opt::$lang],$v['short_text_'.Def\Opt::$lang]);
                       }else{$nuggets_txt.=$this->unitTemplates($v['img'],$v['img_alt_'.Def\Opt::$lang],$v['img_title_'.Def\Opt::$lang],$v['id'],$v['price'],$v['hit'],$v['kind'],$this->ves[Def\Opt::$lang],$v['cap_'.Def\Opt::$lang],$v['short_text_'.Def\Opt::$lang]);}
                   break;
+
+                  case 4:
+                      $kind=json_decode($v['kind']);
+                      $cool_drink_txt.=$this->unitTemplatesKind2($v['img'],$v['img_alt_'.Def\Opt::$lang],$v['img_title_'.Def\Opt::$lang],$v['id'],$v['price'],$v['hit'],$kind,$this->vesH2O[Def\Opt::$lang],$v['cap_'.Def\Opt::$lang],$v['short_text_'.Def\Opt::$lang]);
               }
           }
       }else{Def\Route::$module404=true;}
@@ -73,7 +70,11 @@ class IndexContent{
 
       Def\Opt::$main_content.='<div id="nuggets-section"><h2>'.$lng::$caption_nuggets[Def\Opt::$lang].'</h2></div><div class="tile dwfe maxw">'.$nuggets_txt.'</div>';
 
-      Def\Opt::$main_content.='<div id="beverages-section"><h2>'.$lng::$caption_beverages[Def\Opt::$lang].'</h2></div><br><br><br><br><br><br><br><br>
+      Def\Opt::$main_content.='<div id="beverages-section"><h2>'.$lng::$caption_beverages[Def\Opt::$lang].'</h2></div><h3>'.$this->cool_drink[Def\Opt::$lang].'</h3>
+<div class="tile dwfe maxw">'.$cool_drink_txt.'</div>
+<h3>'.$this->hot_drink[Def\Opt::$lang].'</h3>
+<div class="tile dwfe maxw">'.$hot_drink_txt.'</div>
+
 
 <script type="text/javascript">
     function selectUnit(el){
