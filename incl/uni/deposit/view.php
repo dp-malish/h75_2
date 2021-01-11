93f87504cdaca71603f99292f3b18b095dc8c466
Александр <admin@dp-malish.com>
2021-01-11 11:20:30 +0200

    burger 0.0.0.3

--- incl/uni/deposit/view.php
+++ incl/uni/deposit/view.php
@@ -15,6 +15,11 @@ class View{
 
         Def\Opt::$main_content.=UniDef\OptUni::CURRENCY[0][0];
 
+        $DB=new Def\SQLi();
+        $res=$DB->strSQL('SELECT bank,target FROM depoosit WHERE link='.$DB->realEscapeStr(Def\Route::$uri_parts[$uri_part]));
+
+
+
 
 
     }
