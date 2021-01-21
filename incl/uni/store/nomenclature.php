41df30aadeaa76d77b488e890f656d27930fdd89
Александр <admin@dp-malish.com>
2021-01-21 14:57:21 +0200

    burger 0.0.0.3

---
+++ incl/uni/store/nomenclature.php
@@ -0,0 +1,12 @@
+<?php
+namespace incl\uni\Store;
+
+use lib\Def as Def;
+use incl\uni\Def as UniDef;
+
+class Nomenclature{
+
+
+
+
+}
--- sql/store/nomenclature.sql
+++ sql/store/nomenclature.sql
@@ -1,34 +1,24 @@
 CREATE TABLE IF NOT EXISTS nomenclature(
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(130) NULL,
-  manufacturer SMALLINT NULL,
+ id INT(11) NOT NULL AUTO_INCREMENT,
+
+ heading TINYINT NOT NULL COMMENT 'Заголовок TINYINT',
+ category TINYINT NULL COMMENT 'Категория TINYINT',
+
+ `name` VARCHAR(255) NULL,
+ manufacturer SMALLINT NULL COMMENT 'Производитель',/*производитель*/
+ `mpn` varchar(64) NULL COMMENT 'Manufacturer Part Number штрихкоды',
+
+ provider SMALLINT NULL COMMENT 'Поставщик',
+ provider_code INT NULL COMMENT 'Код поставщика',
 
 
-  user_group_id INT NOT NULL,
-  `username` VARCHAR(20) NULL,
-  `password` VARCHAR(40) NULL,
-  `salt` VARCHAR(9) NULL,
-  `firstname` VARCHAR(32) NULL,
-  `lastname` VARCHAR(32) NULL,
-  patronymic VARCHAR(32) NULL,
-  `image` VARCHAR(255) NULL,
-  `code` VARCHAR(40) NULL,
-  `ip` VARCHAR(40) NULL,
-  `status` TINYINT(1) DEFAULT 1,
-  user_id_referral INT(11) NULL,
-  city VARCHAR(40) NULL,
-  new_mail INT NULL,/*новая почта*/
 
-  level_star_client SMALLINT DEFAULT 3,/*отношение клиента*/
-  note VARCHAR(255) NULL,
-
-  `date_added` DATETIME DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `tel` (`tel` ASC),
-  UNIQUE INDEX `email` (`email` ASC))
-  ENGINE=InnoDB
-  AUTO_INCREMENT = 1
-  DEFAULT CHARACTER SET = utf8;
+ `date_added` DATETIME DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY (`user_id`),
+ UNIQUE INDEX `tel` (`tel` ASC),
+ UNIQUE INDEX `email` (`email` ASC))
+ ENGINE=InnoDB AUTO_INCREMENT=1
+DEFAULT CHARACTER SET=utf8;
 
 /*DROP TABLE sota.user;*/
 --desc dict
